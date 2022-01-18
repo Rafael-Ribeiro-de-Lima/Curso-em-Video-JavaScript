c7d81cb232dbdbd1c6f081566a8ecee94315eef8
Rafael Ribeiro de Lima <65238978+Rafael-Ribeiro-de-Lima@users.noreply.github.com>
2022-01-18 17:08:04 -0300

    aulas repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var i = 1
+
+do {
+    console.log(`Passo ${i}`)
+    i ++
+} while (i <= 10)
+
+
+/* 
+    while (i <= 10){
+    console.log(`Passo ${i}`)
+    i ++
+}
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+console.log('Vai começar...')
+
+for (var c=1; c<=10; c++){
+    console.log(`Passo ${c}`)
+}
+
+console.log('Fim!')
+
+/* 
+
+for (início; teste; incremento){
+    função
+}
+*/
